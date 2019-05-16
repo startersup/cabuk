4c7010a5e5bde3924039f7ebd625728a948312f9
Saicharan <saicharan14996@gmail.com>
2019-05-16 15:22:58 +0530

    Add files via upload

---
+++ script/auto.js
@@ -0,0 +1,153 @@
+ var placeSearch, autocomplete, autocomplete2, autocomplete3, autocomplete4;
+
+    function initAutocomplete() {
+
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */
+            (document.getElementById('autocomplete')), {
+                types: ['geocode'],
+                componentRestrictions: {
+                    country: 'uk'
+                }
+            });
+
+
+
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete.addListener('place_changed', fillInAddress);
+
+
+        autocomplete2 = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */
+            (document.getElementById('autocomplete2')), {
+                types: ['geocode'],
+                componentRestrictions: {
+                    country: 'uk'
+                }
+            });
+
+
+
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete2.addListener('place_changed', fillInAddress);
+
+
+        autocomplete3 = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */
+            (document.getElementById('autocomplete3')), {
+                types: ['geocode'],
+                componentRestrictions: {
+                    country: 'uk'
+                }
+            });
+
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete3.addListener('place_changed', fillInAddress);
+
+
+        autocomplete4 = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */
+            (document.getElementById('autocomplete4')), {
+                types: ['geocode'],
+                componentRestrictions: {
+                    country: 'uk'
+                }
+            });
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete4.addListener('place_changed', fillInAddress);
+
+
+    }
+
+
+    function fillInAddress() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
+        var place = autocomplete2.getPlace();
+
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
+
+        var place = autocomplete3.getPlace();
+
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
+
+        var place = autocomplete4.getPlace();
+
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+$(document).ready(function() {
+  $('#media').carousel({
+    pause: true,
+    interval: false,
+  });
+});
